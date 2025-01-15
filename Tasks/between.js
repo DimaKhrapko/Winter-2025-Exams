5265c6bef31c8fdda1c8518a6cfc8cbe34d889f7
DimaKhrapko <khrapko2007@gmail.com>
2025-01-15 16:48:19 +0200

    between.js step 3; Remove useless if-else block

--- Tasks/between.js
+++ Tasks/between.js
@@ -5,18 +5,13 @@
 const getValueBetween = (str, prefix, suffix) => {
   let i = str.indexOf(prefix);
   if (i === -1) return '';
-  else {
-    const k = i + prefix.length;
-    str = str.substring(k);
-    if (suffix) {
-      i = str.indexOf(suffix);
-      if (i === -1) {
-        return '';
-      } else {
-        str = str.substring(0, i);
-      }
+  const k = i + prefix.length;
+  str = str.substring(k);
+  if (suffix) {
+    i = str.indexOf(suffix);
+    if (i === -1) return '';
+    str = str.substring(0, i);
     }
-  }
   return str;
 };
 

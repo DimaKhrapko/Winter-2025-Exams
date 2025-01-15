7dde00dfa720b1ace2c4fd1b2974c816dc563439
DimaKhrapko <khrapko2007@gmail.com>
2025-01-15 17:10:19 +0200

    day.js step 3; Used .findIndex instead of for loop

--- Tasks/day.js
+++ Tasks/day.js
@@ -5,13 +5,8 @@
 const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseDay = (day) => {
-  let i;
-  for (i = 0; i < DAYS.length; i++) {
-    if (day.startsWith(DAYS[i])) {
-      return i + 1;
-    }
-  }
-  return -1;
+  const index = DAYS.findIndex(d => day.startsWith(d))
+  return index + 1 || -1;
 };
 
 module.exports = parseDay;

7c205c90692c99d206dd96ed76726f4f15694347
DimaKhrapko <khrapko2007@gmail.com>
2025-01-15 20:29:24 +0200

    refactor days constant name

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,10 +2,10 @@
 // Get day number
 'use strict';
 
-const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseDay = (day) => {
-  const index = DAYS.findIndex(d => day.startsWith(d))
+  const index = days.findIndex((d) => day.startsWith(d));
   return index + 1 || -1;
 };
 
